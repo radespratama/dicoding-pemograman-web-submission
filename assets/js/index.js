f98d77faebdc6c84156e8c5d44af512af58d9f82
radespratama <puturades89@gmail.com>
2022-09-12 12:00:21 +0800

    edition-3 done

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,25 +1,24 @@
-const navigationBar = document.getElementById("header-nav");
-const navbarMenu = document.getElementById("nav-menus");
-const buttonHidden = document.getElementById("nav-hidden");
-const buttonShow = document.getElementById("nav-show");
+const navbarMenu = document.getElementById("sidebar");
+const navbarNavigation = document.querySelector(".s-header");
+const buttonHidden = document.getElementById("btn-close");
+const buttonShow = document.getElementById("btn-toggler");
 
 buttonShow.addEventListener("click", () => {
-  navbarMenu.classList.toggle("right-0");
-  navbarMenu.classList.remove("right-100");
+  navbarMenu.classList.toggle("show");
+  navbarMenu.classList.remove("hide");
 });
 
 buttonHidden.addEventListener("click", () => {
-  navbarMenu.classList.toggle("right-100");
-  navbarMenu.classList.remove("right-0");
+  navbarMenu.classList.remove("show");
+  navbarMenu.classList.toggle("hide");
 });
 
 window.onscroll = () => {
   if (window.scrollY > 30) {
-    navigationBar.classList.add("shadow-sm");
+    navbarNavigation.classList.add("shadow-sm");
   } else {
-    navigationBar.classList.remove("shadow-sm");
+    navbarNavigation.classList.remove("shadow-sm");
   }
 };
 
 document.getElementById("year").append(new Date().getFullYear());
-
